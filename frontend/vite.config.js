a7502fcf2187372ca9a71737d34cdb0ddc23eda4
sudheer8177 <sudheerkumar77773@gmail.com>
2026-04-10 00:20:17 +0530

    Update frontend package name to 'curio' and add axios alias in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      axios: path.resolve(__dirname, './node_modules/axios/dist/browser/axios.cjs'),
     },
   },
   server: {
